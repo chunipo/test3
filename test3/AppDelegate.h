e8de580b326a48b1bc7cc675ae09282fe232cac8
weixiaobao <ptah@ptahdeiMac.local>
2016-11-30 00:30:11 -0800

    1

---
+++ test3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test3
+//
+//  Created by ptah on 16/11/30.
+//  Copyright © 2016年 Ptah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test3
+//
+//  Created by ptah on 16/11/30.
+//  Copyright © 2016年 Ptah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
